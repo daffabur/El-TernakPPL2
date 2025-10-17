92549bb9ee374c66634e5cf88cf092afe8d13a94
umaraziz10 <112994604+umaraziz10@users.noreply.github.com>
2025-10-18 04:48:34 +0700

    fix: change the way load .env file

--- backend-el-ternak/initializers/checkEnv.go
+++ backend-el-ternak/initializers/checkEnv.go
@@ -2,12 +2,19 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
-func LoadEnvVariables()  {
-	if err := godotenv.Load(); err != nil {
-		log.Fatal("Gagal mangambil file .env")
+func LoadEnvVariables() {
+	if os.Getenv("GO_ENV") != "production" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error: Gagal memuat file .env di mode development")
+		}
+		log.Println("File .env berhasil dimuat (mode development)")
+	} else {
+		log.Println("Berjalan dalam mode produksi (menggunakan variabel dari environment)")
 	}
 }
